5753e7bead4c6c72366d3a6bbc81215dde4b9ca0
ttherezi <titouan.therezien@edu.univ-fcomte.fr>
2022-11-21 15:30:41 +0100

    commit

--- CoursPerrot/Rupture.java
+++ CoursPerrot/Rupture.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
+import javax.swing.plaf.basic.BasicTabbedPaneUI.TabSelectionHandler;
 
 public class Rupture {
 
     /**
      * affiche un tableau mis en parametre
+     * 
      * @param tab
      */
     static void afficheTab(int[] tab) {
@@ -15,31 +17,95 @@ public class Rupture {
         System.out.println("]");
     }
 
-
     /**
-     * renvoi un tableau entier 1D des indices de début de séquence croissante (quand tab[i]<=tab[i+1])
-     * @param args
+     * affiche un tableau mis en parametre
+     * 
+     * @param tab
      */
-    public static void main(String[] args)
-    {
+    static void afficheTab2D(int[][] tab) {
 
-        int [] tabValeur = {1, 2, 5, 7, 2, 6, 0, 5, 2, 4, 6, 7, 8, 9, 3, 4, 6, 1, 2, 7, 8, 9, 4, 2, 3, 1, 5, 9, 7, 1, 6, 6, 3};
-        afficheTab(tabValeur);
+        System.out.print("Tableau: [ " );
+        for (int j = 0; j < tab.length; j++) {
 
-        int [] tabresult;
-        
-        int i = 0;int n = 0;
-        while (i<tabValeur.length) {
-            while (tabValeur[i] <= tabValeur[i + 1]) {
-                i++;
+            System.out.print("\n ["+ tab[j][0] );
+
+            for (int i = 1; i < tab[j].length; i++) {
+
+
+                System.out.print("," + tab[j][i]);
             }
-            tabresult[n] = i;
-            n++;
+            System.out.print("]") ;
+
         }
 
+        System.out.println("\n  ]");
+    }
+
+    public static int[] RuptureSimple(int[] tabValeur) {
+
+        int cptSequence = 1;
+
+        for (int i = 1; i < tabValeur.length; i++) {
+            if (tabValeur[i] <= tabValeur[i - 1]) {
+                cptSequence++;
+            }
+        }
+
+        int[] tabresult = new int[cptSequence];
+
+        int numSequence = 1;
+        for (int i = 1; i < tabValeur.length; i++) {
+            if (tabValeur[i] <= tabValeur[i - 1]) {
+                tabresult[numSequence] = i;
+                numSequence++;
+            }
+        }
+        return tabresult;
     }
 
 
+    public static int[][] sequence(int[] tab) {
 
+        int[] tabRupture = RuptureSimple(tab);
+        int[][] tSequence = new int [tabRupture.length][];
+        int taille = tabRupture.length; 
+        for (int i = 0; i < taille -1; i++) {
+            tSequence[i] = new int [tabRupture[i+1]-tabRupture[i]];
+            for (int j = tabRupture[i];j< tabRupture[i+1];j++)
+            {
+                tSequence[i][j - tabRupture[i]] = tab[j];
+            }
+        }
+
+            tSequence[i] = new int [tab.length - tabRupture[taille-1]];
+
+            for (int j = tabRupture[taille-1]; j < taille; j++) {
+                tSequence[taille -1][j-tabRupture[taille-1]] = tab[j];
+            }
+       
+
+        return tSequence;
+    }
+
+ 
+    /**
+     * renvoi un tableau entier 1D des indices de début de séquence croissante
+     * (quand tab[i]<=tab[i+1])
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int[] tabValeur = { 1, 2, 5, 7, 2, 6, 0, 5, 2, 4, 6, 7, 8, 9, 3, 4, 6, 1, 2, 7, 8, 9, 4, 2, 3, 1, 5, 9, 7, 1, 6,
+                6, 3 };
+        afficheTab(tabValeur);
+        int [][] tab2 = {{1,2,4,5,6},{3,5,2,1,6},{5,6,6}};
+
+
+        afficheTab2D(tab2);
+        afficheTab2D(sequence(tabValeur));
+        afficheTab(RuptureSimple(tabValeur));
+
+    }
 
 }
